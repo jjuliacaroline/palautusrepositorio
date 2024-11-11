5492599b29af5c1a793a929fcb0a97a2c8e79064
Julia <j.roukala95@gmail.com>
2024-11-11 13:40:28 +0200

    w2ex10

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,11 @@
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-    
+    def __init__(self, data):
+        self.name = data.get("name")
+        self.team = data.get("team")
+        self.nationality = data.get("nationality")
+        self.goals = data.get("goals")
+        self.assists = data.get("assists")
+
     def __str__(self):
-        return self.name
+        return f"{self.name} team {self.team} goals {self.goals} assists {self.assists}"
+
