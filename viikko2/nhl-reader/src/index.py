dfef9f2409e4c371fad3bb87e2cb2a068a65c829
Julia <j.roukala95@gmail.com>
2024-11-11 13:58:39 +0200

    w2ex11

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -6,6 +6,7 @@ def main():
     response = requests.get(url).json()
 
     players = []
+    fin_players = []
 
     for player_dict in response:
         player = Player(player_dict)
@@ -15,7 +16,13 @@ def main():
 
     for player in players:
         if player.nationality == "FIN":
-            print(player)
+            fin_players.append(player)
+
+    fin_players.sort(key=lambda x: x.goals + x.assists, reverse=True)
+
+    for player in fin_players:
+        print(f"{player.name:<20} {player.team:3} goals: {player.goals:2} + assists: {player.assists:2} = {player.goals + player.assists:<3}")
+
 
 if __name__ == "__main__":
     main()
